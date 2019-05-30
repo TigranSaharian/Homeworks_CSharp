147f4f54f6e81aa6c71df47848caafda29dcbf93
TigranSaharian <TigranSaharian@users.noreply.github.com>
2019-05-30 16:25:28 +0400

    Error Hnadling

---
+++ Error_handling/Error_handling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 20;
+                int b = 0;
+                double s = a / b;
+                Console.WriteLine(s);
+            }
+            catch (Exception erroe)
+            {
+                Console.WriteLine(erroe.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
